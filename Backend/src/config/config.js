c30bbf398f7f8de3cc1df69c1ec453ba1e87cf9f
Joe Thorpe <joey515@live.com.au>
2020-06-18 18:56:28 +1000

    fixed error

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    port: process.env.PORT || 8081,
+    port: 8081,
     db: {
-        database: process.env.DB_NAME || 'todo',
-        user: process.env.DB_USER ||'todo',
-        password: process.env.DB_PASS ||'todo',
+        database: 'todo',
+        user: 'todo',
+        password: 'todo',
         options: {
-            host: process.env.HOST || "database",
-            dialect: process.env.DIALECT ||"mysql",
+            host: "database",
+            dialect:  "mysql",
         }
     }
 }
